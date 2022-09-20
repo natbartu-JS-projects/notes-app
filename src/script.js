57bf80f7a4115a4fddd10d88fdcf4af564abf718
natacha <nat_bartu@hotmail.com>
2022-09-20 11:39:33 +1000

    style the add button

--- src/script.js
+++ src/script.js
@@ -2,6 +2,10 @@ const addBtn = document.getElementById("add");
 
 const notes = JSON.parse(localStorage.getItem("notes"));
 
+if (notes) {
+  notes.forEach((note) => addNewNote(note));
+}
+
 addBtn.addEventListener("click", () => addNewNote());
 
 function addNewNote(text = "") {
